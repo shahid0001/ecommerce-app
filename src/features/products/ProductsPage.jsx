b838585132c63d0830aa3f2bcf3109b41464a1e2
shahid0001 <shahidrazzaq241@gmail.com>
2026-02-18 16:12:25 +0500

    update import

--- src/features/products/ProductsPage.jsx
+++ src/features/products/ProductsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import ProductCard from './ProductCard.jsx'
+import ProductCard from './productCard'
 import SearchBar from '../../components/SearchBar'
 import { 
   fetchProductsAsync, 
